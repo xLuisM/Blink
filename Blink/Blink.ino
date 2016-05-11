9dea02eb31ffcf32b16017df6ae7b3cb4a04a843
Luis Miguel <xluismx@gmail.com>
2016-05-11 18:35:54 +0200

    uptade delay
    and defines.

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,8 +1,12 @@
+#define Delay_1s 1000
+
 void setup() {
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);   
+  digitalWrite(13, HIGH);
+  delay(Delay_1s);
   digitalWrite(13, LOW);   
+  delay(Delay_1s);
 }
